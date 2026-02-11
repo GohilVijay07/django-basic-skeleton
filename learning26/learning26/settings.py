24efc98db50e20b189a47f82a73d9160f98df1d9
Gohil Vijay <gohilvijay53949@gmail.com>
2026-02-11 20:42:39 +0530

    Refactor database settings to use environment variables for user and password

--- learning26/learning26/settings.py
+++ learning26/learning26/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,8 +81,8 @@ DATABASES = {
     'default': {
        'ENGINE':'django.db.backends.postgresql',
        'NAME':'learning26',
-       'USER':'postgres',
-       'PASSWORD':'Admin@123',
+       'USER':os.getenv('POSTGRES_USER', 'postgres'),
+       'PASSWORD':os.getenv('POSTGRES_PASSWORD', 'Admin@123'),
        'HOST':'localhost',
        'PORT':'5432',
     }
